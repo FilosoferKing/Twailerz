dd601419ebb502d206aa329907bc975953236dc1
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-05 12:37:37 -0700

    pulling html from apple

--- apple.js
+++ apple.js
@@ -2,4 +2,25 @@
  * Created by Andy on 8/5/15.
  */
 var movie_list = [];
-$(document).ready()
+var aData;
+$(document).ready(function () {
+    $.jGFeed('http://trailers.apple.com/trailers/home/rss/newtrailers.rss',
+        function (feeds) {
+            // Check for errors
+            if (!feeds) {
+                // there was an error
+                return false;
+            }
+            movie_list = feeds.entries;
+            console.log(movie_list);
+            // do whatever you want with feeds here
+        }, 20);
+});
+
+$(document).ready(function () {
+    $('#load').click(function () {
+        for (var i = 0; i < movie_list.length; i++) {
+            $('#main').append(movie_list[i].content);
+        }
+    })
+})
