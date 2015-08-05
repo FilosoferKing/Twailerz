dc5ed895c17388852db94dac1d520277c6d7eaf7
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-05 15:19:39 -0700

    cleaned up, update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Twailerz project
+<h4>Team TAT's Awesome Project</h4>
+<h5>Our Goal</h5>
+Provide a new take on showing popular opinion on upcomming movies.
+<h5>Our Methods</h5>
+
+- Pull current movies from Apple.
+
+- Present it in a nice format.
+
+- Show a youtube trailer, movie info and synopsis.
+
+- Present a current Twitter feed involving that movie.
--- apple.js
+++ apple.js
@@ -2,7 +2,6 @@
  * Created by Andy on 8/5/15.
  */
 var movie_list = [];
-var aData;
 $(document).ready(function () {
     $.jGFeed('http://trailers.apple.com/trailers/home/rss/newtrailers.rss',
         function (feeds) {
@@ -24,27 +23,9 @@ $(document).ready(function () {
                 class: 'col-xs-6 btn btn-warning',
                 id: movie_list[i].title,
                 html: movie_list[i].title
-            })
+            });
             $('.container').append(moviedivs);
 
         }
     })
-})
-
-
-/*
-$(document).ready(function(){
-    $('#utube').click(function(){
-        $.ajax({
-            dataType:'json',
-            type:"GET",
-            url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
-            query: 'cats',
-            maxResults: 5,
-            success: function(results){
-                console.log(results);
-            }
-        })
-    })
-})
-    */
+});
