8b7e399dfa4b2d434bca1d4a988d707bbe6be397
filosoferking <filosoferking@yahoo.com>
2015-08-06 09:10:39 -0700

    js files combined to main.js

--- apple.js
+++ apple.js
@@ -1,36 +1,3 @@
-/**
- * Created by Andy on 8/5/15.
- */
-var movie_list = [];
-$(document).ready(function () {
-    $.jGFeed('http://trailers.apple.com/trailers/home/rss/newtrailers.rss',
-        function (feeds) {
-            if (!feeds) {
-                return false;
-            }
-            movie_list = feeds.entries;
-            console.log(movie_list);
-            for (var i = 0; i < movie_list.length; i++) {
-                console.log('looping');
-                var str = movie_list[i]['content'];
-                var newstring = str.substring(str.lastIndexOf("img src=") + 9, str.lastIndexOf(".jpg") + 4);
-                var outerdiv = $('<div>', {
-                    class: 'col-sm-3 outerdiv'
-                });
-                var innerdiv = $('<div>', {
-                    class: 'col-sm-12 innerdiv'
-                });
-                var movieImg = $('<img>', {
-                    class: 'images',
-                    src: newstring,
-                    title: movie_list[i]['title'],
-                    'data-toggle': "modal",
-                    'data-target': "#myModal"
-                });
-                $(innerdiv).append(movieImg);
-                $(outerdiv).append(innerdiv);
-                $('#content').append(outerdiv);
-            }
-        }, 20);
-});
+
+
 
